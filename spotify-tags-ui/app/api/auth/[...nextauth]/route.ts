d44e1aabdb75c7a8228bafb102a66c257d6f9d87
ablil <ablil@proton.me>
2024-12-04 20:05:49 +0100

    feat: Add authentication
    Adds basic authentication using NextAuth.  Includes a
    login page and protects the application's main route.

---
+++ spotify-tags-ui/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,26 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        password: { label: "password", type: "password" },
+      },
+      // eslint-disable-next-line
+      async authorize(credentials, _): Promise<any> {
+        if (credentials?.password ===process.env.PASSWORD) {
+          return {
+            username: "admin",
+          }
+        }
+        return null
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/hmmm'
+  },
+  debug: true,
+})
+export { handler as GET, handler as POST }
